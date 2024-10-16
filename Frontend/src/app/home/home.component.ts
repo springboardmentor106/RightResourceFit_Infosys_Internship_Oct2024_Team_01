76f3ad6310e1218b67459a04905dd4fb44a8330c
akashbalodi <akashbalodi17@gmail.com>
2024-10-16 17:33:56 +0530

    angular

--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
---
+++ Frontend/src/app/recruiter/recruiter.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-recruiter',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './recruiter.component.html',
+  styleUrl: './recruiter.component.css'
+})
+export class RecruiterComponent {
+
+}
