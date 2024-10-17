90b58d20e72fbbe0734a88a912a2d8e38f8729b8
nivedhithaannasankar <nivedhithaannasankar@gmail.com>
2024-10-17 06:44:44 +0530

    sign up

--- Frontend/src/app/sign-up/sign-up.component.ts
+++ Frontend/src/app/sign-up/sign-up.component.ts
@@ -1,12 +1,28 @@
+
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
-  standalone: true,
   imports: [],
-  templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.css'
+    templateUrl: './sign-up.component.html',
+    styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent {
+    user = {
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    };
 
+    get passwordMismatch() {
+        return this.user.password !== this.user.confirmPassword && this.user.confirmPassword.length > 0;
+    }
+
+    onSubmit() {
+        if (!this.passwordMismatch && this.user.name && this.user.email && this.user.password) {
+            console.log('Form Submitted', this.user);
+        }
+    }
 }
